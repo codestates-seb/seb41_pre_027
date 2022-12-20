a8bc0d0c9ee75d38064df745db8544200986a106
mason <masonlee7629@gmail.com>
2022-12-21 07:14:36 +0900

    Feat : 질문 목록 보기 구현

--- Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/question/service/QuestionService.java
+++ Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/question/service/QuestionService.java
@@ -7,6 +7,9 @@ import com.codestates.pre027.PreProjectStackOverFlow.question.repository.Questio
 import java.time.LocalDateTime;
 import java.util.Optional;
 import javax.swing.text.html.Option;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -37,6 +40,11 @@ public class QuestionService {
     public Question findQuestion(long questionId) {
         Question findQuestion = findVerifiedQuestion(questionId);
         return findQuestion;
+    }
+
+    public Page<Question> findQuestions(int page, int size) {
+        return questionRepository.findAll(PageRequest.of(page, size,
+            Sort.by("questionId").descending()));
     }
 
     public Question findVerifiedQuestion(long questionId) {
