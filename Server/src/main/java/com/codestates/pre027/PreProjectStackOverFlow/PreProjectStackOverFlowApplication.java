83e717d7414ac39dbb180840b54802fa5a7c8386
oheadnah <kanmini@naver.com>
2022-12-21 11:58:57 +0900

    Fix : 애너테이션 추가

--- Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/PreProjectStackOverFlowApplication.java
+++ Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/PreProjectStackOverFlowApplication.java
@@ -2,8 +2,10 @@ package com.codestates.pre027.PreProjectStackOverFlow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PreProjectStackOverFlowApplication {
 
     public static void main(String[] args) {
