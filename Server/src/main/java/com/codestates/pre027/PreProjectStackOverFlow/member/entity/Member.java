a2c191e851e718193ef54849899015ce163fabb3
oheadnah <kanmini@naver.com>
2022-12-22 09:20:51 +0900

    Fix : email 에 unique 속성 부여

--- Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/member/entity/Member.java
+++ Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/member/entity/Member.java
@@ -25,7 +25,7 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
-    @Column(nullable = false)
+    @Column(nullable = false , unique = true)
     private String email;
 
     @Column(nullable = false)
