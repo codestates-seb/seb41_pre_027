6cc6588496ab002d987ed4f808d2e9d59a9c925e
DryLeonhard <ygangmin@gmail.com>
2022-12-23 09:27:10 +0900

    Fix : 답변 등록시 tokenId로 memberId 등록, 답변 삭제시 tokenId와 답변 등록한 memberId 대조하여 권한확인

--- Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/answer/controller/AnswerController.java
+++ Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/answer/controller/AnswerController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,11 +34,12 @@ public class AnswerController {
     }
 
     @PostMapping("/questions/{quest-id}/answers")
-    public ResponseEntity postAnswer(
+    public ResponseEntity postAnswer(@RequestHeader(name = "Authorization") String token,
         @PathVariable("quest-id") @Positive long questId,
         @Valid @RequestBody AnswerDto.Post answerPostDto){
         Answer answer = answerService.createAnswer(answerMapper.answerPostDto_to_Answer(answerPostDto),
-            questId);
+            questId,
+            jwtTokenizer.getMemberId(token));
         AnswerDto.Response response = answerMapper.answer_to_AnswerResponseDto(answer);
         return new ResponseEntity<>(
                 response,
@@ -45,10 +47,12 @@ public class AnswerController {
     }
 
     @PatchMapping("/answers/{answer-id}")
-    public ResponseEntity patchAnswer(@PathVariable("answer-id") @Positive long answerId,
+    public ResponseEntity patchAnswer(@RequestHeader(name = "Authorization") String token,
+        @PathVariable("answer-id") @Positive long answerId,
         @Valid @RequestBody AnswerDto.Patch answerPatchDto){
         answerPatchDto.setAnswerId(answerId);
-        Answer answer = answerService.updateAnswer(answerMapper.answerPatchDto_to_Answer(answerPatchDto));
+        Answer answer = answerService.updateAnswer(answerMapper.answerPatchDto_to_Answer(answerPatchDto),
+            jwtTokenizer.getMemberId(token));
         AnswerDto.Response response = answerMapper.answer_to_AnswerResponseDto(answer);
         return new ResponseEntity<>(
             response,
@@ -80,8 +84,9 @@ public class AnswerController {
     }
 
     @DeleteMapping("/answers/{answer-id}")
-    public ResponseEntity deleteAnswer(@PathVariable("answer-id") @Positive long answerId){
-        answerService.deleteAnswer(answerId);
+    public ResponseEntity deleteAnswer(@RequestHeader(name = "Authorization") String token,
+        @PathVariable("answer-id") @Positive long answerId){
+        answerService.deleteAnswer(answerId,jwtTokenizer.getMemberId(token));
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
