5fadc0ad536284e6d2b73658e8f3f073c83995e5
DryLeonhard <ygangmin@gmail.com>
2022-12-23 10:33:26 +0900

    Fix : 모르고 건드림

--- Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/answer/controller/AnswerController.java
+++ Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/answer/controller/AnswerController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,7 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@Validated
+@RequestMapping("/api")
 public class AnswerController {
     private final AnswerService answerService;
     private final AnswerMapper answerMapper;
