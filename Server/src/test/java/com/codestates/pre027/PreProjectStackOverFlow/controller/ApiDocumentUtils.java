aecbab1f45b4b8d93f20e1a7be0d3a7dd395e7b9
oheadnah <kanmini@naver.com>
2022-12-26 10:46:48 +0900

    Feat : RestDocs 문서화 구현

---
+++ Server/src/test/java/com/codestates/pre027/PreProjectStackOverFlow/controller/ApiDocumentUtils.java
@@ -0,0 +1,17 @@
+package com.codestates.pre027.PreProjectStackOverFlow.controller;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+public interface ApiDocumentUtils {
+
+    static OperationRequestPreprocessor getRequestPreProcessor() {
+        return preprocessRequest(prettyPrint());
+    }
+
+    static OperationResponsePreprocessor getResponsePreProcessor() {
+        return preprocessResponse(prettyPrint());
+    }
+}
