213eb9f36bcbbf452bb99beb03aceabf70880f56
oheadnah <kanmini@naver.com>
2022-12-27 13:19:56 +0900

    Feat : 로그인시 MemberId 반환하게 구현완료

--- Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/auth/dto/LoginDto.java
+++ Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/auth/dto/LoginDto.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 //클라이언트가 전송한 Username/Password 정보를 SecurityFilter 에서
 //사용할 수 있도록 역직렬화(Deserialization)하기 위한 DTO 클래스입니다.
 @Getter
-@Setter
 public class LoginDto {
 
     private String username;
