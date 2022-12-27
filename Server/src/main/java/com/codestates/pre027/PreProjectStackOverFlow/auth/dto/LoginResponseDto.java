23df39e86f1231cba9c4974c7840b338e470fb27
oheadnah <kanmini@naver.com>
2022-12-27 13:19:40 +0900

    Feat : 로그인시 MemberId 반환하게 구현완료

---
+++ Server/src/main/java/com/codestates/pre027/PreProjectStackOverFlow/auth/dto/LoginResponseDto.java
@@ -0,0 +1,11 @@
+package com.codestates.pre027.PreProjectStackOverFlow.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginResponseDto {
+    private long memberId;
+    private long memberImage;
+}
