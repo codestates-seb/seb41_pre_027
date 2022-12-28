9e7c1593d36715831d92fc2e08da4e6067723199
FordangIT <fordang0819@gmail.com>
2022-12-28 15:28:59 +0900

    Feat: 라우터, 상태관리

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import useFetch from './utils/useFetch';
+import useScrollTop from './utils/useScrollTop';
+import GlobalStyle from './assets/style/GlobalStyle';
+
+import QuestionList from './Pages/Questions/QuestionList';
+import AskQuestions from './Pages/Questions/AskQuestions';
+import QuestionDetail from './Pages/Questions/QuestionDetail';
+import EditQuestion from './Pages/Questions/EdItQuestion';
 
 function App() {
-  return <div>스택오버플로우 클론</div>;
+  const [boards, isPending, error, setBoard] = useFetch(
+    'http://localhost:4000/boards/'
+  );
+  useScrollTop();
+  return (
+    <div>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<QuestionList boards={boards} isPending={isPending} />}
+          />
+          <Route path="/create" element={<AskQuestions boards={boards} />} />
+          <Route
+            path="/boards/:id"
+            element={<QuestionDetail setBoard={setBoard} />}
+          />
+          <Route
+            path="/edit/:id"
+            element={<EditQuestion boards={boards} setBoard={setBoard} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
