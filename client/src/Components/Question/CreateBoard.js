1a65bcd15e68814a9762efc11a49c37ac2089b99
FordangIT <fordang0819@gmail.com>
2022-12-29 22:01:28 +0900

    Feat:ask생성 화면 수정

--- client/src/Components/Question/CreateBoard.js
+++ client/src/Components/Question/CreateBoard.js
@@ -1,10 +1,9 @@
-import ToastAnswer from '../toast/ToastAnswer';
 import styled from 'styled-components';
 import ask from '../../assets/images/ask.png';
 
-import Input from '../Input';
 import useInput from '../../utils/useInput';
 import { fetchCreate } from '../../utils/api';
+import ToastAsk from '../toast/ToastAsk';
 
 const Container = styled.div`
   background-color: #f8f9f9;
@@ -23,14 +22,28 @@ const Head = styled.div`
 `;
 const Description = styled.div`
   border: 1px solid #afd3ef;
+  border-radius: 3px;
   background-color: #ebf4fb;
-  padding: 40px;
+  padding: 25px 20px;
+  h3 {
+    font-size: 20px;
+    font-weight: 400;
+  }
+  .description__head--detail {
+    margin-top: 15px;
+    font-size: 15px;
+    line-height: 1.4rem;
+    margin-bottom: 15px;
+  }
+  h4 {
+    margin-bottom: 13px;
+  }
+  li {
+    margin-left: 15px;
+    line-height: 1.4rem;
+  }
 `;
 
-// const Input = styled.input`
-//   width: 90%;
-//   margin: 0px 40px;
-// `;
 const Main = styled.div`
   margin-left: 7rem;
   margin-right: 40rem;
@@ -41,14 +54,14 @@ const Main = styled.div`
   }
 `;
 function CreateBoard({ boards }) {
-  const [title, setTitle] = useInput('');
-  const [body, setBody] = useInput('');
+  //const [title, setTitle] = useInput('');
+  //const [body, setBody] = useInput('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = { title, body };
-    fetchCreate('http://localhost:4000/boards/', data);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const data = { title, body };
+  //   fetchCreate('/api/questions/posting', data);
+  // };
   return (
     <>
       <Container>
@@ -62,35 +75,38 @@ function CreateBoard({ boards }) {
         </Head>
         <Main>
           <Description>
-            <div>
+            <div className="description__head">
               <h3>Writing a good question</h3>
-              <br></br>
-              <span>
-                You’re ready to ask a programming-related question and this form
-                will help guide you through the process.
-              </span>
-              <br></br>
-              <span>
-                Looking to ask a non-programming question? See the topics here
-                to find a relevant site.
-              </span>
-              <br></br>
-              <h4>steps</h4>
-              <ul>
-                <li>Summarize your problem in a one-line title.</li>
-                <li>Describe your problem in more detail.</li>
-                <li>
-                  Describe what you tried and what you expected to happen.
-                </li>
-                <li>
-                  Add “tags” which help surface your question to members of the
-                  community.
-                </li>
-                <li>Review your question and post it to the site.s</li>
-              </ul>
+              <div className="description__head--detail">
+                <span>
+                  You’re ready to ask a programming-related question and this
+                  form will help guide you through the process.
+                </span>
+                <div>
+                  <span>
+                    Looking to ask a non-programming question? See the topics
+                    here to find a relevant site.
+                  </span>
+                </div>
+              </div>
+              <div className="description__tail">
+                <h4>Steps</h4>
+                <ul>
+                  <li>Summarize your problem in a one-line title.</li>
+                  <li>Describe your problem in more detail.</li>
+                  <li>
+                    Describe what you tried and what you expected to happen.
+                  </li>
+                  <li>
+                    Add “tags” which help surface your question to members of
+                    the community.
+                  </li>
+                  <li>Review your question and post it to the site.s</li>
+                </ul>
+              </div>
             </div>
           </Description>
-          <form onSubmit={handleSubmit}>
+          {/* <form onSubmit={handleSubmit}>
             <div className="content">
               <div className="content__title">Title</div>
               <div className="content__title__description">
@@ -107,10 +123,10 @@ function CreateBoard({ boards }) {
               </h5>
               <Input label={'내용'} value={setBody}></Input>
               {/* <ToastAnswer handleSubmit={handleSubmit} /> */}
-            </div>
+          {/* </div>
             <button onSubmit={handleSubmit}>제출</button>
-          </form>
-          <ToastAnswer />
+          </form> */}
+          <ToastAsk />
         </Main>
       </Container>
     </>
