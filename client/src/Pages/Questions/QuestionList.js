9f6fd208e1b306f7388329e33b494fd9f13e3f6c
FordangIT <fordang0819@gmail.com>
2022-12-30 12:33:56 +0900

    Feat: 게시물list 수정

--- client/src/Pages/Questions/QuestionList.js
+++ client/src/Pages/Questions/QuestionList.js
@@ -1,11 +1,11 @@
-import BoardList from '../../Components/Question/BoardList';
-import Loading from '../../Components/Loading';
-function QuestionList({ boards, isPending }) {
-  return (
-    <>
-      {isPending && <Loading />}
-      {boards && <BoardList boards={boards} isPending={isPending} />}
-    </>
-  );
-}
-export default QuestionList;
+// import BoardList from '../../Components/Question/BoardList';
+// import Loading from '../../Components/Loading';
+// function QuestionList({ boards, isPending }) {
+//   return (
+//     <>
+//       {isPending && <Loading />}
+//       {boards && <BoardList boards={boards} isPending={isPending} />}
+//     </>
+//   );
+// }
+// export default QuestionList;
