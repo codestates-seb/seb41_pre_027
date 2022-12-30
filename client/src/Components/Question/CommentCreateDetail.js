c355726e7c216551cd29d41f8db1800dccb5dabb
FordangIT <fordang0819@gmail.com>
2022-12-30 12:28:27 +0900

    Feat: 댓글 조회 기능 추가

--- client/src/Components/Question/CommentCreateDetail.js
+++ client/src/Components/Question/CommentCreateDetail.js
@@ -3,7 +3,6 @@ import { fetchCreateComment } from '../../utils/api';
 import useInput from '../../utils/useInput';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
 const CommentPost = styled.div`
   .comment__post {
     border: none;
@@ -17,107 +16,83 @@ const CommentPost = styled.div`
     }
   }
 `;
-const CommentRead = styled.div``;
+const CommentRead = styled.div`
+  .comment__read--text {
+    border-bottom: 1px solid #f1f2f3;
+    margin-bottom: 18px;
+    padding: 20px;
+  }
+`;
 const Text = styled.div``;
+
 function CommentCreateDetail() {
   const { id } = useParams();
-  const [commentContent, setCommentContent] = useState('');
-  const [settingComment, setSettingComment] = useState('');
-  const handleInputChange = (event) => {
-    setCommentContent(event.target.value);
-    console.log(commentContent);
-  };
+  //comment 가져오기
+  const comment = useFetch(`/api/questions/${id}/comments`);
+  const getcomment = comment[0];
+  //comment 생성하기
+  const [commentContent, setCommentContent] = useInput('');
 
-  useEffect(() => {
-    console.log(commentContent);
-  }, [commentContent]);
-
-  const handleAnswerSubmit = () => {
-    fetch(`/api/questions/${id}/comments`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-        //Authorization: token,
-      },
-      body: JSON.stringify({
-        commentContent,
-      }),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw Error('could not fetch the data for that resource');
-        }
-        window.location.reload();
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
-  };
-  const onCheckEnter = (e) => {
+  const submitForm = (e) => {
     if (e.key === 'Enter') {
-      handleAnswerSubmit();
-      setCommentContent('');
-      console.log('post');
+      console.log(commentContent.value);
+      fetchCreateComment(
+        `/api/questions/${id}/comments`,
+        id,
+        commentContent.value
+      );
     }
   };
-
-  useEffect(() => {
-    fetch(
-      `http://ec2-43-201-73-28.ap-northeast-2.compute.amazonaws.com:8080/users/me`, //여기 작성해야 함
-      {
-        method: 'GET',
-        headers: {
-          'Content-type': 'application/json',
-          //Authorization: initialToken,
-        },
-      }
-    )
-      .then((res) => {
-        if (!res.ok) {
-          throw Error('could not fetch the data for that resource');
-        }
-        return res.json();
-      })
-      .then((json) => {
-        console.log('json', json);
-        setCommentContent(json.text);
-      })
-      .catch((err) => console.log(err));
-  }, []);
 
   return (
     <>
       <CommentRead>
         <div className="comment__read">
-          {/* 지금 여기 map 부분때문에 input에 enter 치지도 않았는데 새로고침된다.
-          {commentContent &&
-            commentContent.map((el) => {
-              <div className="comment__read--id" key={el.id}>
-                <div className="comment__read--text">{el.text}</div>
-              </div>;
-            })} */}
+          {getcomment &&
+            getcomment.map((el) => {
+              return (
+                <div className="comment__read--id" key={el.commentId}>
+                  <div className="comment__read--text">{el.text}</div>
+                </div>
+              );
+            })}
         </div>
       </CommentRead>
       <CommentPost>
-        <div className="comment__post">
-          <input
-            type="text"
-            placeholder="Add a comment"
-            value={commentContent}
-            onChange={handleInputChange}
-            onKeyDown={onCheckEnter}
-          ></input>
-        </div>
+        <form onSubmit={submitForm}>
+          <div className="comment__post">
+            <input
+              type="text"
+              placeholder="Add a comment"
+              {...commentContent}
+              onKeyDown={(e) => submitForm(e)}
+            ></input>
+          </div>
+        </form>
       </CommentPost>
     </>
   );
 }
 export default CommentCreateDetail;
 
-/* <div>
-        {comments.map((comment) => {
-          <div className="comment__preview" key={comment.id}>
-            <div className="comment__list">{comment.comment}</div>
-          </div>;
-        })}
-      </div> */
+// const handleAnswerSubmit = () => {
+//   fetch(`/api/questions/${id}/comments`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-type': 'application/json',
+//       //Authorization: token,
+//     },
+//     body: JSON.stringify({
+//       commentContent,
+//     }),
+//   })
+//     .then((res) => {
+//       if (!res.ok) {
+//         throw Error('could not fetch the data for that resource');
+//       }
+//       window.location.reload();
+//     })
+//     .catch((error) => {
+//       throw new Error(error);
+//     });
+// };
