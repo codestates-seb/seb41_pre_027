e0e9ed667ddc719b56c76e2e3669639cd6d86b90
FordangIT <fordang0819@gmail.com>
2022-12-31 01:33:36 +0900

    Feat:답변 페이지

--- client/src/Components/Question/AnswerDetail.js
+++ client/src/Components/Question/AnswerDetail.js
@@ -1,6 +1,13 @@
 import ToastAnswer from '../toast/ToastAnswer';
 import styled from 'styled-components';
-
+import { useParams } from 'react-router';
+import useFetch from '../../utils/useFetch';
+const AnswerPost = styled.div``;
+const AnswerRead = styled.div`
+  .answer__read {
+    margin-top: 30px;
+  }
+`;
 const Container = styled.div`
   .answer__label {
     margin-top: 50px;
@@ -16,15 +23,33 @@ const Container = styled.div`
   }
 `;
 function AnswerDetail() {
+  const { id } = useParams();
+  const answer = useFetch(`/api/questions/${id}/answers`);
+  const getanswer = answer[0];
+  console.log(getanswer);
   return (
     <>
       <Container>
-        <div className="answer__label">
-          <span>Your Answer</span>
-        </div>
-        <div className="answer__content">
-          <ToastAnswer />
-        </div>
+        <AnswerRead>
+          <div className="answer__read">
+            {getanswer &&
+              getanswer.map((el) => {
+                return (
+                  <div className="answer__read--id" key={el.answerId}>
+                    <div className="answer__read--text">{el.text}</div>
+                  </div>
+                );
+              })}
+          </div>
+        </AnswerRead>
+        <AnswerPost>
+          <div className="answer__label">
+            <span>Your Answer</span>
+          </div>
+          <div className="answer__content">
+            <ToastAnswer />
+          </div>
+        </AnswerPost>
       </Container>
     </>
   );
