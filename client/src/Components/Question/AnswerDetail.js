42d7a375e504ec81591293d6b9b2d3e5cc9a0471
FordangIT <fordang0819@gmail.com>
2022-12-31 21:38:29 +0900

    Feat: 답변 crud fetch

--- client/src/Components/Question/AnswerDetail.js
+++ client/src/Components/Question/AnswerDetail.js
@@ -2,6 +2,12 @@ import ToastAnswer from '../toast/ToastAnswer';
 import styled from 'styled-components';
 import { useParams } from 'react-router';
 import useFetch from '../../utils/useFetch';
+import useInput from '../../utils/useInput';
+import {
+  fetchCreateAnswer,
+  fetchDeleteAnswer,
+  fetchPatchAnswer,
+} from '../../utils/api';
 const AnswerPost = styled.div``;
 const AnswerRead = styled.div`
   .answer__read {
@@ -27,6 +33,34 @@ function AnswerDetail() {
   const answer = useFetch(`/api/questions/${id}/answers`);
   const getanswer = answer[0];
   console.log(getanswer);
+
+  const [answerContent, bindAnswerContent, resetAnswerContent] = useInput('');
+  const submitForm = (e) => {
+    e.preventDefault();
+    fetchCreateAnswer(
+      process.env.REACT_APP_DB_HOST + `api/questions/${id}/answers`,
+      id,
+      {
+        text: answerContent,
+      }
+    );
+    resetAnswerContent();
+  };
+  const deleteForm = (answerId) => {
+    alert('삭제');
+    fetchDeleteAnswer(
+      process.env.REACT_APP_DB_HOST + `/api/answers/${answerId}`,
+      id
+    );
+  };
+  const patchForm = (answerId) => {
+    alert('수정');
+    fetchPatchAnswer(
+      process.env.REACT_APP_DB_HOST + `/api/answers/${answerId}`,
+      id
+      //데이터 들어가야함
+    );
+  };
   return (
     <>
       <Container>
@@ -37,6 +71,13 @@ function AnswerDetail() {
                 return (
                   <div className="answer__read--id" key={el.answerId}>
                     <div className="answer__read--text">{el.text}</div>
+                    <button onClick={(e) => patchForm(el.commentId)}>
+                      수정
+                    </button>
+                    <button onClick={(e) => deleteForm(el.commentId)}>
+                      삭제
+                    </button>
+                    {console.log('이거야', el.answerId)}
                   </div>
                 );
               })}
